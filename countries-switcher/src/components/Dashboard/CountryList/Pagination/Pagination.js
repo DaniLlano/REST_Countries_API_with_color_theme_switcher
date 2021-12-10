c3e73451b7b7b2407dd4f05facad03ae0500f213
Dani <danillano97@gmail.com>
2021-12-10 16:00:40 -0300

    pagination

--- countries-switcher/src/components/Dashboard/CountryList/Pagination/Pagination.js
+++ countries-switcher/src/components/Dashboard/CountryList/Pagination/Pagination.js
@@ -45,7 +45,62 @@ const paginationOfFirstAndLastPage = (
                 } : undefined}
                 className={ currentPage === 0 ? styles.isActive : undefined}
             >
+                1
+            </button>
+            <button
+                onClick={ currentPage + 1 === 1
+                    ? () => {
+                        setCurrentPage(totalPages - 1);
+                        scroll(scrollTo);
+                    }
+                    : undefined
+                }
+                className={currentPage + 1 === totalPages ? styles.isActive : undefined}
+            >
+                {totalPages}
             </button>
         </>
-    )
+    );
+};
+
+const paginationForSecondAndOneBeforeLastPage = (
+    currentPage,
+    totalPages,
+    setCurrentPage,
+    scrollTo
+) => {
+    return (
+        <>
+            <button
+                onClick={() => {
+                    setCurrentPages(totalPages - 1);
+                    scroll(scrollTo);
+                }}
+            >
+                {totalPages}
+            </button>
+        </>
+    );
+};
+
+const paginationMiddleSectionCreator = (
+    currentPage,
+    totalPages,
+    setCurrentPage,
+    scrollTo
+) => {
+    if (totalPages === 1) {
+        return <button className={styles.isActive}>1</button>
+    }
+    switch (currentPage + 1) {
+        case 1:
+        case totalPages: {
+            return paginationOfFirstAndLastPage (
+                currentPage,
+                totalPages,
+                setCurrentPage,
+                scrollTo
+            );
+        }
+    }
 }
